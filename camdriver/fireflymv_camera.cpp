80ba59f8acc1147a42b8f5cb65024d12f0d95ab3
Nikolas Davis <nikdavis@pdx.edu>
2014-03-30 21:55:22 -0700

    Added PID exposure control to camera drivers in ./camdriver. It calculates a region of interest's (ROI's) mean and controls it against a setpoint. This ROI is currently the lower ~1/3 of the image. The setpoint is 90. These are currently both hard set in the driver. The PID could use better tuning, but works for now.
    Former-commit-id: 19993fbb19e9636998333144d433e365df8a3519 [formerly 816a7179f9f16895fce1aeaf12278b5d40bc45f6]
    Former-commit-id: 3a30b81eb68752a53b34630293f6f0be0c4e3e1d
    Former-commit-id: 9ca52445cf7471de8f072c69e404285313766198
    Former-commit-id: 1575274b970174ac3f626df14de1e85f9d94ccf0

--- camdriver/fireflymv_camera.cpp
+++ camdriver/fireflymv_camera.cpp
@@ -14,6 +14,19 @@ FireflyMVCamera::FireflyMVCamera() {
     fc2EmbeddedImageInfo embeddedInfo;
     initialized = 1;
     frames = 0;
+    fps = 30;
+    /* PID initialization */
+    statePID = 1;
+    errorPID[0] = errorPID[1] = errorPID[2] = 0;
+    setPoint = 90.0;
+    integError = 0;
+    derivError = 0;
+    dt = 1.0 / 30.0;
+    Ku = 0.14;
+    Pu = 0.45;
+    Kp = 0.45 * Ku;
+    Ki = 1.7 * Kp / Pu;
+    Kd = Kp * Pu / 8.0;
 
     error = fc2CreateContext( &context );
     if ( error != FC2_ERROR_OK )
@@ -77,6 +90,18 @@ FireflyMVCamera::FireflyMVCamera() {
         printf( "Error in fc2CreateImage: %d\n", error );
         initialized = -1;
     }
+
+    if(initialized == 1) {
+        /* Setup registers for our manual PID shutter control */
+        error = fc2WriteRegister(context, 0x81C, 0x820000A8);     // Set shutter value
+        error = fc2WriteRegister(context, 0x800, 0x82000001);     // Auto-brightness off
+        error = fc2WriteRegister(context, 0x804, 0x82000007);     // Auto-exposure off, set exposure value
+        error = fc2WriteRegister(context, 0x820, 0x82000010);     // Auto-gain off
+        if( error != FC2_ERROR_OK ) {
+            printf("Error in register initialization! %d\n", error);
+            initialized = -1;
+        }
+    }
 }
 
 
@@ -129,10 +154,79 @@ int FireflyMVCamera::grabFrame(cv::Mat &retFrame) {
     else {
         cv::Mat rawImage(480, 640, CV_8UC1, image.pData, 0);
         retFrame = rawImage.clone();
+        if(statePID == 1) processPID(retFrame);
         frames++;
         return 0;
     }
 }
+
+
+int FireflyMVCamera::enablePID(void) {
+    statePID = 1;
+    return 0;
+}
+
+
+int FireflyMVCamera::disablePID(void) {
+    statePID = 0;
+    return 0;
+}
+
+
+int FireflyMVCamera::readShutter(unsigned int * value) {
+    /* Read the value of the shutter register, 0x81C. The value
+     * comprises bits 20:31, or as we read it bits 11:0.            */
+    fc2Error error;
+    error = fc2ReadRegister(context, 0x81C, value);
+    *value &= 0xFFF;
+    if( error != FC2_ERROR_OK )     return -1;
+    else                            return 0;
+}
+
+int FireflyMVCamera::setShutter(unsigned int value) {
+    /* RMW the value of the shutter register, 0x81C. The value
+     * comprises bits 20:31, or as we read it bits 11:0.            */
+    fc2Error error;
+    unsigned int temp;
+    error = fc2ReadRegister(context, 0x81C, &temp);
+    if( error != FC2_ERROR_OK )     return -1;
+    temp &= 0xFFFFF000;         // Clear old value
+    temp |= (value & 0xFFF);    // Set new value
+    // Write back
+    error = fc2WriteRegister(context, 0x81C, temp);
+    return 0;
+}
+
+
+int FireflyMVCamera::processPID(cv::Mat &frame) {
+    unsigned int shutter = 0;
+    float shutterFl = 0;
+    float p, i, d;
+    cv::Mat roadFrame = frame(cv::Range(270, CAMERA_HEIGHT), cv::Range(0, CAMERA_WIDTH));
+    cv::Scalar roadMu = cv::mean(roadFrame);
+    errorPID[2] = errorPID[1];
+    errorPID[1] = errorPID[0];
+    errorPID[0] = setPoint - roadMu(0);
+    /* Use trap method to compute error */
+    integError += 0.5 * (errorPID[0] + errorPID[1]) * dt;
+    derivError = (errorPID[0] - errorPID[1]) / (dt * 1);     
+    p = Kp * errorPID[0];
+    i = Ki * integError;
+    d = Kd * derivError;
+    //i = 0;
+    //d = 0;
+    /* Note: to make this work I had to disable auto-exposure, -brightness, and -gain.
+     * I also had to set ABSOLUTE mode OFF on each */
+    readShutter(&shutter);
+    /* Update shutter w/ basic PID */
+    shutterFl = (float)shutter + p + i + d;
+    if(shutterFl > 500) shutterFl = 500;
+    if(shutterFl < 0) shutterFl = 0;
+    shutter = (unsigned int) shutterFl;
+    setShutter(shutter);
+    return 0;
+}
+
 
 
 
